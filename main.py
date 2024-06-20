a3cd9058e2d6d049b4fc3eed56b06f206198d7a5
ksg-dev <ksg.dev.data@gmail.com>
2024-06-20 12:19:14 -0500

    send formatted articles via twilio whatsapp api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,6 +9,9 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "K11SJ0TEQSVHIK5P"
 NEWS_API_KEY = "035c473d7ce44654a8aa06bdd3a45458"
+
+TWILIO_ACCOUNT_SID = "AC753e79b3830c4a1cf58b84b208cdffdf"
+TWILIO_AUTH_TOKEN = "a5f0a5c361eebf139d681d06f3e4773d"
 
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -19,65 +23,55 @@ stock_params = {
     "apikey": STOCK_API_KEY
 }
 
-# response = requests.get(STOCK_ENDPOINT, params=stock_params)
-# data = response.json()["Time Series (Daily)"]
-# data_list = [value for (key, value) in data.items()]
-# yesterday_data = data_list[0]
-# yesterday_closing = yesterday_data["4. close"]
-# print(yesterday_closing)
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing = yesterday_data["4. close"]
 
 
 # Get the day before yesterday's closing stock price
-# day_before_yesterday_data = data_list[1]
-# day_before_yesterday_close = day_before_yesterday_data["4. close"]
-# print(day_before_yesterday_close)
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_close = day_before_yesterday_data["4. close"]
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
-# difference = abs(float(yesterday_closing) - float(day_before_yesterday_close))
-# print(difference)
-#
-# # Get percentage difference in price between closing price yesterday and closing price the day before yesterday.
-# diff_percent = (difference / float(yesterday_closing)) * 100
-# print(diff_percent)
+difference = abs(float(yesterday_closing) - float(day_before_yesterday_close))
+
+
+# Get percentage difference in price between closing price yesterday and closing price the day before yesterday.
+diff_percent = (difference / float(yesterday_closing)) * 100
 
 
 # If TODO4 percentage is greater than 5 then print("Get News").
-# if diff_percent > 5:
-news_params = {
-    "apiKey": NEWS_API_KEY,
-    "qInTitle": COMPANY_NAME,
-}
-news_response = requests.get(NEWS_ENDPOINT, params=news_params)
-articles = news_response.json()["articles"]
-print(articles)
+if diff_percent > 5:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
 # STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-#TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
-
-#TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
+# Use Python slice operator to create a list that contains the first 3 articles.
 three_articles = articles[:3]
-# print(three_articles)
 
-    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
-    #to send a separate message with each article's title and description to your phone number. 
 
-#TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+# STEP 3: Use twilio.com/docs/sms/quickstart/python
+# to send a separate message with each article's title and description to your phone number.
+
+# Create a new list of the first 3 article's headline and description using list comprehension.
 formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
-print(formatted_articles)
 
-#TODO 9. - Send each article as a separate message via Twilio. 
+# Send each article as a separate message via Twilio.
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-
-
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+for article in formatted_articles:
+    message = client.messages.create(
+        body=article,
+        from_="whatsapp:+14155238886",
+        to="whatsapp:+13344078777"
+    )
+    print(message.status)
 
