a8adefa6fbb0ad4be745b91937c2bcc1fe28d3f1
ksg-dev <ksg.dev.data@gmail.com>
2024-06-20 12:07:59 -0500

    get articles from news api

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = "K11SJ0TEQSVHIK5P"
+NEWS_API_KEY = "035c473d7ce44654a8aa06bdd3a45458"
 
 # STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -18,43 +19,52 @@ stock_params = {
     "apikey": STOCK_API_KEY
 }
 
-response = requests.get(STOCK_ENDPOINT, params=stock_params)
-data = response.json()["Time Series (Daily)"]
-data_list = [value for (key, value) in data.items()]
-yesterday_data = data_list[0]
-yesterday_closing = yesterday_data["4. close"]
-print(yesterday_closing)
+# response = requests.get(STOCK_ENDPOINT, params=stock_params)
+# data = response.json()["Time Series (Daily)"]
+# data_list = [value for (key, value) in data.items()]
+# yesterday_data = data_list[0]
+# yesterday_closing = yesterday_data["4. close"]
+# print(yesterday_closing)
 
 
 # Get the day before yesterday's closing stock price
-day_before_yesterday_data = data_list[1]
-day_before_yesterday_close = day_before_yesterday_data["4. close"]
-print(day_before_yesterday_close)
+# day_before_yesterday_data = data_list[1]
+# day_before_yesterday_close = day_before_yesterday_data["4. close"]
+# print(day_before_yesterday_close)
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
-difference = abs(float(yesterday_closing) - float(day_before_yesterday_close))
-print(difference)
+# difference = abs(float(yesterday_closing) - float(day_before_yesterday_close))
+# print(difference)
+#
+# # Get percentage difference in price between closing price yesterday and closing price the day before yesterday.
+# diff_percent = (difference / float(yesterday_closing)) * 100
+# print(diff_percent)
 
-# Get percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing)) * 100
-print(diff_percent)
 
-
-#TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-if diff_percent > 5:
-    print("Get News")
-    ## STEP 2: https://newsapi.org/ 
-    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# If TODO4 percentage is greater than 5 then print("Get News").
+# if diff_percent > 5:
+news_params = {
+    "apiKey": NEWS_API_KEY,
+    "qInTitle": COMPANY_NAME,
+}
+news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+articles = news_response.json()["articles"]
+print(articles)
+# STEP 2: https://newsapi.org/
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 #TODO 6. - Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 
 #TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint: https://stackoverflow.com/questions/509211/understanding-slice-notation
-
+three_articles = articles[:3]
+# print(three_articles)
 
     ## STEP 3: Use twilio.com/docs/sms/quickstart/python
     #to send a separate message with each article's title and description to your phone number. 
 
 #TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+formatted_articles = [f"Headline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+print(formatted_articles)
 
 #TODO 9. - Send each article as a separate message via Twilio. 
 
